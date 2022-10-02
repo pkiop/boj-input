bacc219bd0e9eacba6209e1f1109705269fee84d
pkiop <pkiopb@gmail.com>
2022-10-02 15:39:33 +0900

    Feat: init

---
+++ fe/src/App.js
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+const Layout = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+function App() {
+  return <Layout></Layout>;
+}
+
+export default App;
